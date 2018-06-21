fe45dd39e4f77e932280c2851ca9b0b26be313ee
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-06-21 11:00:22 +0100

    Apply changes to Slider

--- static/src/javascripts/projects/common/modules/audio/Slider.js
+++ static/src/javascripts/projects/common/modules/audio/Slider.js
@@ -4,23 +4,22 @@ import { styled, Component } from '@guardian/dotcom-rendering/packages/guui';
 
 const Scrubber = styled('div')(({ left }) => ({
   backgroundColor: '#ffffff',
-  border: '2px solid #000000',
   borderRadius: '100%',
-  height: '12px',
+  height: '14px',
   left: `${left}px`,
   position: 'absolute',
-  top: '50%',
+  top: '50%', 
   transform: 'translate(-50%, -50%)',
-  width: '12px',
+  width: '14px',
 }));
 
 export default function Slider({ min, max, value }) {
   return (
-    <Scrubber
+    <Scrubber 
       left={value}
-      role="slider"
-      aria-valuenow={value}
-      aria-valuemin={min}
+      role="slider" 
+      aria-valuenow={value} 
+      aria-valuemin={min} 
       aria-valuemax={max}
       />
   );
